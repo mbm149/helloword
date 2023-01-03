a7d0f46855841a6d6758d92162411c67a1426159
mbm149 <105382530+mbm149@users.noreply.github.com>
2023-01-02 21:11:04 -0700

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -1,55 +1,37 @@
-#########################################################
-####Create the Storage bucket in the US multi-region#####
-#########################################################
-
-#resource "random_id" "bucket_prefix" {
-#  byte_length = 8
-#}
-
-resource "google_storage_bucket" "mbrandmcloudresume" {
-  name          = "mbrandmcloudresume01"
-  location      = "US"
-  storage_class = "NEARLINE"
-
-  uniform_bucket_level_access = false
-  
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
-  cors {
-    origin          = ["http://mbrandmcloudresume.com"]
-    method          = ["GET","POST"]
-    response_header = ["*"]
-    max_age_seconds = 3600
-  }
-}
-#########################################################
-####Upload index.html to mbrandmcloudresume.com bucket###
-#########################################################
-# Upload a simple index.html page to the bucket
-resource "google_storage_bucket_object" "indexpage" {
-  name         = "index.html"
-  source      = "index.html"
-  bucket       = google_storage_bucket.mbrandmcloudresume.id
-
-}
-# Upload a simple 404 / error page to the bucket
-resource "google_storage_bucket_object" "errorpage" {
-  name         = "404.html"
-  source      = "404.html"
-  content_type = "text/html"
-  bucket       = google_storage_bucket.mbrandmcloudresume.id
+provider "google" {
+    project = "my-project-githubaction"
 }
 
-#########################################################
-##########Make the files public to the internet##########
-#########################################################
-##  
+resource "google_cloud_run_service" "default" {
+    name     = "SERVICE"
+    location = "us-central1"
 
-resource "google_storage_bucket_access_control" "public_rule" {
-  bucket = google_storage_bucket.mbrandmcloudresume.id
-  role   = "READER"
-  entity = "allUsers"
+    metadata {
+      annotations = {
+        "run.googleapis.com/client-name" = "terraform"
+      }
+    }
+
+    template {
+      spec {
+        containers {
+          image = "gcr.io/my-project-githubaction/app"
+        }
+      }
+    }
+ }
+
+ data "google_iam_policy" "noauth" {
+   binding {
+     role = "roles/run.invoker"
+     members = ["allUsers"]
+   }
+ }
+
+ resource "google_cloud_run_service_iam_policy" "noauth" {
+   location    = google_cloud_run_service.default.location
+   project     = google_cloud_run_service.default.project
+   service     = google_cloud_run_service.default.name
+
+   policy_data = data.google_iam_policy.noauth.policy_data
 }
-
